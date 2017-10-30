f000cbc8f0c892e1630e663a4e2ccfdf9fe888e9
dgole <dgole100@gmail.com>
2017-10-30 14:19:04 -0600

    Update demo1.py

--- python/myCode/demo1.py
+++ python/myCode/demo1.py
@@ -1,16 +1,17 @@
 import time
 from neopixel import *
 import lib
+import numpy
 
 # Create NeoPixel object with appropriate configuration.
 strip = Adafruit_NeoPixel(lib.LED_COUNT, lib.LED_PIN, lib.LED_FREQ_HZ, lib.LED_DMA, lib.LED_INVERT, lib.LED_BRIGHTNESS, lib.LED_CHANNEL, lib.LED_STRIP)
 strip.begin()
+
 while True:
-	for i in range(strip.numPixels()):
-		strip.setPixelColor(i, Color(255,0,0))
-	strip.show()
-	for i in range(strip.numPixels()):
-		strip.setPixelColor(i, Color(0,0,0))
-	strip.show()
-	time.sleep(0.1)
-	
+	for n in range(0, 60):
+		a = numpy.zeros(lib.LED_COUNT,3)
+		a[0:n, 0] = 255  
+		for i in range(lib.LED_COUNT):
+			strip.setPixelColor(i, Color(a[i,0],a[i,1],a[i,2]))
+		strip.show()
+		
