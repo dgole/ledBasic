6029c75aae9cb8b063beb5f381c19c6f56b26d44
dgole <dgole100@gmail.com>
2017-10-30 14:23:34 -0600

    Update demo1.py

--- python/myCode/demo1.py
+++ python/myCode/demo1.py
@@ -7,11 +7,10 @@ import numpy
 strip = Adafruit_NeoPixel(lib.LED_COUNT, lib.LED_PIN, lib.LED_FREQ_HZ, lib.LED_DMA, lib.LED_INVERT, lib.LED_BRIGHTNESS, lib.LED_CHANNEL, lib.LED_STRIP)
 strip.begin()
 
+frame=0
 while True:
-	for n in range(0, 60):
-		a = numpy.zeros([lib.LED_COUNT,3])
-		a[0:n, 0] = 255  
-		for i in range(lib.LED_COUNT):
-			strip.setPixelColor(i, Color(a[i,0],a[i,1],a[i,2]))
-		strip.show()
-		
+	rval = frame % 255
+	for i in range(lib.LED_COUNT):
+		strip.setPixelColor(i, Color(rval, 0, 0)
+	strip.show()
+	frame+=1
