67d05b68e3e3f555f14d7951ec731cfa4547ed89
dgole <dgole100@gmail.com>
2017-10-30 14:42:10 -0600

    Update demo1.py

--- python/myCode/demo1.py
+++ python/myCode/demo1.py
@@ -8,13 +8,12 @@ strip = Adafruit_NeoPixel(lib.LED_COUNT, lib.LED_PIN, lib.LED_FREQ_HZ, lib.LED_D
 strip.begin()
 
 def setPixelsWithArray(strip, a):
-	a = a.astype(int)
 	for i in range(strip.numPixels()):
 		strip.setPixelColor(i, Color(a[i,0], a[i,1], a[i,2]))
 	strip.show()
 
 while True:
-	a = numpy.zeros(strip.numPixels(), 3)
+	a = numpy.zeros([strip.numPixels(), 3])
 	for n in range(0, strip.numPixels()):
 		a[0:n, 0] = n * (strip.numPixels()/255)
 		setPixelsWithArray(strip, a)
