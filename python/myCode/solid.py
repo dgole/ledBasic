6024b43890947e0ea1423a5b96898b28a700afd3
dgole <dgole100@gmail.com>
2017-10-30 16:06:36 -0600

    Update and rename clear.py to solid.py

--- python/myCode/solid.py
+++ python/myCode/solid.py
@@ -1,8 +1,15 @@
 import time
 from neopixel import *
 import lib
+import sys
+
+c1 = sys.argv[1]
+c2 = sys.argv[2]
+c3 = sys.argv[3]
 
 strip = Adafruit_NeoPixel(lib.LED_COUNT, lib.LED_PIN, lib.LED_FREQ_HZ, lib.LED_DMA, lib.LED_INVERT, lib.LED_BRIGHTNESS, lib.LED_CHANNEL, lib.LED_STRIP)
 strip.begin()
-for i in range(strip.numPixels()): strip.setPixelColor(i, Color(0,0,0))
+
+for i in range(strip.numPixels()): strip.setPixelColor(i, Color(c1,c2,c3))
+
 strip.show()
