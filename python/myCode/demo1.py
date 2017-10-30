da755bed83104a68badefac38bda8d6a89ddbf1e
dgole <dgole100@gmail.com>
2017-10-30 15:24:10 -0600

    Update demo1.py

--- python/myCode/demo1.py
+++ python/myCode/demo1.py
@@ -22,13 +22,13 @@ width  = 5
 for i in range(center-width, center+width+1):
 	distance = np.absolute(i-center)
 	distanceNorm = float(distance)/float(width)
-	a[i,0] = 1.0 - distanceNorm
+	a[i,0] = 1.0 - distanceNorm + 0.1
 a = np.square(a)
 a = normalizeArray(a)
 
 plusOrMinus = 1
 while True:
-	if center == 59 or center == 0: plusOrMinus*=-1
+	if center == 59-width or center == 0+width: plusOrMinus*=-1
 	a = np.roll(a, plusOrMinus, axis=0)
 	setPixelsWithArray(strip, a)
 	time.sleep(0.1)
