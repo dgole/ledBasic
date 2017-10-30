5f6af11a5993b3a35a2122cded567ef8e9a198df
dgole <dgole100@gmail.com>
2017-10-30 16:00:08 -0600

    Create blue.py

---
+++ python/myCode/blue.py
@@ -0,0 +1,8 @@
+import time
+from neopixel import *
+import lib
+
+strip = Adafruit_NeoPixel(lib.LED_COUNT, lib.LED_PIN, lib.LED_FREQ_HZ, lib.LED_DMA, lib.LED_INVERT, lib.LED_BRIGHTNESS, lib.LED_CHANNEL, lib.LED_STRIP)
+strip.begin()
+for i in range(strip.numPixels()): strip.setPixelColor(i, Color(0,0,255))
+strip.show()
