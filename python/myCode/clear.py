2260d02a74c3824d56a84b9f70ef27d3b7457c05
dgole <dgole100@gmail.com>
2017-10-30 16:00:30 -0600

    Update clear.py

--- python/myCode/clear.py
+++ python/myCode/clear.py
@@ -4,5 +4,5 @@ import lib
 
 strip = Adafruit_NeoPixel(lib.LED_COUNT, lib.LED_PIN, lib.LED_FREQ_HZ, lib.LED_DMA, lib.LED_INVERT, lib.LED_BRIGHTNESS, lib.LED_CHANNEL, lib.LED_STRIP)
 strip.begin()
-for i in range(strip.numPixels()): strip.setPixelColor(i, Color(255,0,0))
+for i in range(strip.numPixels()): strip.setPixelColor(i, Color(0,0,0))
 strip.show()
