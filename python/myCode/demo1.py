c21da6cc5aa27ee68c4f794bf275844eee5c2531
dgole <dgole100@gmail.com>
2017-10-30 14:59:12 -0600

    Update demo1.py

--- python/myCode/demo1.py
+++ python/myCode/demo1.py
@@ -3,6 +3,7 @@ from neopixel import *
 import lib
 import numpy
 
+
 # Create NeoPixel object with appropriate configuration.
 strip = Adafruit_NeoPixel(lib.LED_COUNT, lib.LED_PIN, lib.LED_FREQ_HZ, lib.LED_DMA, lib.LED_INVERT, lib.LED_BRIGHTNESS, lib.LED_CHANNEL, lib.LED_STRIP)
 strip.begin()
@@ -12,8 +13,18 @@ def setPixelsWithArray(strip, a):
 		strip.setPixelColor(i, Color(int(a[i,0]), int(a[i,1]), int(a[i,2])))
 	strip.show()
 
+a = numpy.zeros([strip.numPixels(), 3])
+center = 30
+width  = 5
+for i in range(center-width, center+width+1):
+	distance = numpy.absolute(i-center)
+	distanceNorm = distance/width
+	a[i] = 255 * (1.0/distanceNorm) 
+print(a)
+a = np.square(a)
+print(a)
+	
+frame = 0
 while True:
-	a = numpy.zeros([strip.numPixels(), 3])
-	for val in range(0, 255):
-		a[:, 0] = val
-		setPixelsWithArray(strip, a)
+	setPixelsWithArray(strip, np.roll(a, frame))
+	frame+=1
