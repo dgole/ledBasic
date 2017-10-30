4506a9da06f18583bf5d0c90240d0e2d68e1be9b
dgole <dgole100@gmail.com>
2017-10-30 13:32:06 -0600

    Update lib.py

--- python/myCode/lib.py
+++ python/myCode/lib.py
@@ -67,13 +67,3 @@ def theaterChaseRainbow(strip, wait_ms=50):
 			for i in range(0, strip.numPixels(), 3):
 				strip.setPixelColor(i+q, 0)
 
-
-# Main program logic follows:
-if __name__ == '__main__':
-	# Create NeoPixel object with appropriate configuration.
-	strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL, LED_STRIP)
-	# Intialize the library (must be called once before other functions).
-	strip.begin()
-	for i in range(strip.numPixels()): strip.setPixelColor(i, Color(0,0,0))
-	strip.show()
-
