265498bf724042a352744fe34cb89477151bc878
dgole <dgole100@gmail.com>
2017-11-03 20:10:15 -0600

    Update rainbow.py

--- python/myCode/rainbow.py
+++ python/myCode/rainbow.py
@@ -6,4 +6,5 @@ import lib
 strip = Adafruit_NeoPixel(lib.LED_COUNT, lib.LED_PIN, lib.LED_FREQ_HZ, lib.LED_DMA, lib.LED_INVERT, lib.LED_BRIGHTNESS, lib.LED_CHANNEL, lib.LED_STRIP)
 strip.begin()
 while True:
-	lib.rainbowCycle(strip, wait_ms=20, iterations=5)
+	#lib.rainbowCycle(strip, wait_ms=20, iterations=5)
+	lib.rainbow(strip, wait_ms=20, iterations=1)
