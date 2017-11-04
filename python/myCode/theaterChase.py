c2644dfa56c9c3e02e21def5c47bc9917faf1fc7
dgole <dgole100@gmail.com>
2017-11-03 19:54:26 -0600

    Update theaterChase.py

--- python/myCode/theaterChase.py
+++ python/myCode/theaterChase.py
@@ -6,9 +6,9 @@ import lib
 strip = Adafruit_NeoPixel(lib.LED_COUNT, lib.LED_PIN, lib.LED_FREQ_HZ, lib.LED_DMA, lib.LED_INVERT, lib.LED_BRIGHTNESS, lib.LED_CHANNEL, lib.LED_STRIP)
 strip.begin()
 while True:
-	#lib.theaterChase(strip, Color(127, 127, 127))  # White theater chase
-	lib.theaterChase(strip, Color(127,   0,   0))  # Red theater chase
-	#lib.theaterChase(strip, Color(  0,   0, 127))  # Blue theater chase
+	lib.theaterChase(strip, Color(255, 0, 0))  # Red theater chase
+	lib.theaterChase(strip, Color(0, 255, 0))  # Blue theater chase
+	lib.theaterChase(strip, Color(0, 0, 255))  # Blue theater chase
 	#lib.rainbow(strip)
 	#lib.rainbowCycle(strip)
 	#lib.theaterChaseRainbow(strip)
