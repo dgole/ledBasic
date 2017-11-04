25c8465c3cab4a3180c414609ff3a57674d0dccf
dgole <dgole100@gmail.com>
2017-11-03 19:52:23 -0600

    Create theaterChase.py

---
+++ python/myCode/theaterChase.py
@@ -0,0 +1,14 @@
+import time
+from neopixel import *
+import lib
+
+# Create NeoPixel object with appropriate configuration.
+strip = Adafruit_NeoPixel(lib.LED_COUNT, lib.LED_PIN, lib.LED_FREQ_HZ, lib.LED_DMA, lib.LED_INVERT, lib.LED_BRIGHTNESS, lib.LED_CHANNEL, lib.LED_STRIP)
+strip.begin()
+while True:
+	#lib.theaterChase(strip, Color(127, 127, 127))  # White theater chase
+	#lib.theaterChase(strip, Color(127,   0,   0))  # Red theater chase
+	#lib.theaterChase(strip, Color(  0,   0, 127))  # Blue theater chase
+	#lib.rainbow(strip)
+	#lib.rainbowCycle(strip)
+	#lib.theaterChaseRainbow(strip)
