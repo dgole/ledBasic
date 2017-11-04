c53d49b5206173840a75f677747e86e43bf267b2
dgole <dgole100@gmail.com>
2017-11-03 20:14:50 -0600

    Create colorWipeSym.py

---
+++ python/myCode/colorWipeSym.py
@@ -0,0 +1,11 @@
+import time
+from neopixel import *
+import lib
+
+# Create NeoPixel object with appropriate configuration.
+strip = Adafruit_NeoPixel(lib.LED_COUNT, lib.LED_PIN, lib.LED_FREQ_HZ, lib.LED_DMA, lib.LED_INVERT, lib.LED_BRIGHTNESS, lib.LED_CHANNEL, lib.LED_STRIP)
+strip.begin()
+while True:
+	lib.colorWipeSym(strip, Color(255, 0, 0), wait_ms=30)
+	lib.colorWipeSym(strip, Color(0, 255, 0), wait_ms=30)
+	lib.colorWipeSym(strip, Color(0, 0, 255), wait_ms=30)
